35b382fd7aaa04e8eff7951938b11cae6dde565f
DansRoh <100189192+DansRoh@users.noreply.github.com>
2023-05-11 18:10:54 +0800

    feature(login): "login page refactor"

--- pages/login/login.vue
+++ pages/login/login.vue
@@ -1,28 +1,20 @@
 <template>
-	<view class="page-login" :style="{paddingTop: ptHeight+'px'}">
+	<view class="page-login">
 		<navbar>
-			<view class="df aic c5e" slot="left-box">
-				<van-image width="208rpx" height="40rpx" src="/static/icon/10_waterStewardText.png"></van-image>
-				<view class="cue-divider mr16 ml16">
-				</view>
-				<view class="fs36 c5e">
-					销售端
-				</view>
+			<view slot="content" class="custon-nav-content">
+				<van-image class="left-box" width="208rpx" height="52rpx" src="/static/logo.png"></van-image>
 			</view>
 		</navbar>
 		<van-image width="100vw" height="652rpx" src="/static/images/02_login_banner.png"></van-image>
-		<van-cell-group custom-class="cus-cell-group-class">
-			<van-field title-width="104rpx" @change="phoneNumChange" type="number"
-				custom-style="border: 2rpx solid #828698;margin-bottom:30rpx;border-radius: 8rpx;overflow:hidden;" size="large"
-				label="手机号" label-class="custom-label" :value="phoneNum" clearable>
-			</van-field>
-			<van-field title-width="104rpx" @change="veriCodeChange"
-				custom-style="border: 2rpx solid #828698;padding:0;padding-left:34rpx;align-items:center;border-radius: 8rpx;overflow:hidden;"
-				size="large" label="验证码" label-class="custom-label" :value="veriCode" clearable>
-				<van-button custom-style="width: 208rpx" :disabled="isVericodeBtnDisable" @click="handleClickGetVericodeBtn"
-					type="primary" color="#383838" slot="button">{{vericodeBtnText}}</van-button>
-			</van-field>
-		</van-cell-group>
+		<view class="inp-list">
+			<cus-input type="number" custom-class="cus-input" label="手机号" :value="formData.phone"
+				@input="bindInput('phone', $event)" hasBorder></cus-input>
+			<cus-input type="number" custom-class="cus-input" label="验证码" :value="userInfo.veriCode"
+				@input="bindInput('veriCode',$event)" hasBorder>
+				<van-button custom-style="width: 208rpx" type="primary" color="#383838" slot="right"
+					:disabled="isVericodeBtnDisable" @click="handleClickGetVericodeBtn">{{vericodeBtnText}}</van-button>
+			</cus-input>
+		</view>
 		<van-button class="mt30" type="primary" color="#FFD242" @click="handleClickLogin" :disabled="isDisabledLogin"
 			custom-style="width: 592rpx;font-size:36rpx;height: 112rpx;color:#383838;" round>登录</van-button>
 		<view class="go-register-box">
@@ -37,28 +29,34 @@
 
 <script>
 	import navbar from '@/components/navbar/navbar.vue';
+	import cusInput from '../../components/cus-input/cus-input.vue'
 	export default {
 		components: {
-			navbar
+			navbar,
+			cusInput
 		},
 		data() {
 			return {
-				phoneNum: '',
-				veriCode: '',
+				formData: {
+					phone: '',
+					veriCode: '',
+				},
 				isAgree: false,
 				vericodeBtnText: '获取验证码',
 				isVericodeBtnDisable: false,
-				ptHeight: 60,
-				isDisabledLogin: false,
 			};
 		},
-		onLoad() {
-			const ptHeight = uni.getStorageSync('navHeight')
-			if (ptHeight) {
-				this.ptHeight = ptHeight
+		computed: {
+			isDisabledLogin() {
+				return Object.values(this.formData).some(val => val === '')
 			}
 		},
+		onLoad() {
+		},
 		methods: {
+			bindInput(key, value) {
+				this.formData[key] = value
+			},
 			phoneNumChange(e) {
 				this.phoneNum = e.detail
 			},
@@ -66,24 +64,10 @@
 				this.veriCode = e.detail
 			},
 			handleClickLogin() {
-				if (!this.phoneNum) {
-					uni.showToast({
-						title: '请填写手机号',
-						icon: 'error'
-					})
-					return
-				}
-				if (!this.veriCode) {
-					uni.showToast({
-						title: '请填写验证码',
-						icon: 'error'
-					})
-					return
-				}
 				if (!this.isAgree) {
 					uni.showToast({
 						title: '请阅读并同意协议',
-						icon: 'error'
+						icon: 'none'
 					})
 					return
 				}
@@ -124,30 +108,21 @@
 		display: flex;
 		flex-direction: column;
 		align-items: center;
-
-		.cue-divider {
-			width: 2rpx;
-			height: 34rpx;
-			background-color: #5E5E5E;
+		.custon-nav-content {
+			width: 100%;
+			height: 100%;
+			display: flex;
+			align-items: flex-end;
+			padding-left: 40rpx;
 		}
 
-		.login-poster {
-			width: 100vw;
-			height: 652rpx;
-			border: 1px solid;
-		}
-
-		.cus-cell-group-class {
-			margin: 0 auto;
-			width: 624rpx;
-
-			.custom-label {
-				width: 104rpx;
-				font-size: 28rpx;
-				color: #828698;
+		.inp-list {
+			.cus-input {
+				width: 624rpx;
+				height: 80rpx;
+				margin-top: 30rpx;
 			}
 		}
-
 		.go-register-box {
 			margin-top: 50rpx;
 			display: flex;
